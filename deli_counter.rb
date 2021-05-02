5058a6ab14841e8c409f8c7cb645e46049de5623
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 21:57:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+array = ["Logan", "Avi", "Spencer"]
+
+def line(array)
+  line_array = []
+  
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    array.each_with_index do |customer, index| 
+        line_array.push("#{index + 1}. #{customer}")
+      end 
+      puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(array, new_customer)
+  array.push(new_customer)
+    puts "Welcome, #{new_customer}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+    if array.length == 0 
+      puts "There is nobody waiting to be served!"
+    else
+      serving = array.shift
+      puts "Currently serving #{serving}."
+    end
+end
+
+
+
+
+def interaction(array)
+  counter = 0
+  puts "Welcome to the Deli Counter."
+  puts "Can you please put your name:"
+  while counter <= 5
+   new_customer = gets.chomp
+   take_a_number(array, new_customer)
+   line(array)
+   now_serving(array)
+   counter += 1
+  end
+end
+
+interaction(array)
+
+ 
